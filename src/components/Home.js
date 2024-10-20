2bc40780143f9ef0b0e407af35e97356b8f46465
seaseeraw <seaseeraw12@gmail.com>
2024-10-20 23:07:07 +1100

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import BasicExample from './nav';
+import GridComplexExample from './contact';
+function Home() {
+  return (
+    <div>
+        <BasicExample/>
+        <GridComplexExample/>
+    </div>
+  )
+}
+
+export default Home;
