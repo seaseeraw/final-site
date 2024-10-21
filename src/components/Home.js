4107b9d29539813aaa05bdb210501f478c9845c4
seaseeraw <seaseeraw12@gmail.com>
2024-10-22 09:13:09 +1100

    layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import BasicExample from './nav';
 import GridComplexExample from './contact';
+import BasicExamples from './Content';
+
+import Example from './About';
 function Home() {
   return (
     <div>
         <BasicExample/>
         <GridComplexExample/>
+        <Example/>
+        <BasicExamples/>
     </div>
   )
 }
